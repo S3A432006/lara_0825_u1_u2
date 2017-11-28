9d710abba7ffe8f841e51f989b556cdc60f69461
Jia Yi Lai <anniuu7134@gmail.com>
2017-11-28 16:21:17 +0800

    修改change(),after()指令

--- database/migrations/2017_11_28_081328_add_is_feature_in_posts_table.php
+++ database/migrations/2017_11_28_081328_add_is_feature_in_posts_table.php
@@ -15,7 +15,7 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
-            $table->string('title',500) ->change();
+            $table ->string('title',500) ->change();
             $table ->boolean('is_feature') ->default(false) ->after('content');
         });
     }
