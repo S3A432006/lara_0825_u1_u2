fcfc1adbf34b21419dc79a269d8e6b934e276df8
Jia Yi Lai <anniuu7134@gmail.com>
2017-11-28 18:08:06 +0800

    讓資料有時間序列性、隨機感

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Database\Seeder;
 use App\Post;
+use Carbon\Carbon;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -13,9 +14,13 @@ class PostsTableSeeder extends Seeder
         Post::truncate();
         //單元二-1
         foreach(range(1,20) as $number) {
+            $total=20;
             Post::create([
                 'title' => 'title ' . $number,
                 'content' => 'content ' . $number,
+                'is_feature' => rand(0,1),
+                'created_at' => Carbon::now() ->subDays($total - $number),
+                'updated_at' => Carbon::now() ->subDays($total - $number),
             ]);
         }
     }
